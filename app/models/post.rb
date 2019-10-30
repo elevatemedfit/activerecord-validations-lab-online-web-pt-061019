0a3966f3feb0aaf7fca432648c7853cd9f0a8810
Chris Junker <elevatemedfit@gmail.com>
2019-10-30 22:16:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  
+
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {in: %w(Non-Fiction)}
+  validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
   validate :clickbait?
 
 
